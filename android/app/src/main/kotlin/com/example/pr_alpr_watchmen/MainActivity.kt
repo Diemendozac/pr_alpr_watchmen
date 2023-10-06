1d5002a364358a4f41e832c85cccb83a14a8164e
diemendozac <diemendozac@gmail.com>
2023-10-06 10:47:50 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pr_alpr_watchmen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pr_alpr_watchmen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/plate_scanner_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlateScannerPage extends StatelessWidget {
+  const PlateScannerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
